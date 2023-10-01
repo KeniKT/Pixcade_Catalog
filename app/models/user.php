b8051d0b1b2f37cdf161317fd3483870cfcbf345
alazarlemma02 <alazar03lemma@gmail.com>
2023-10-01 14:12:25 +0300

    added method to create and insert a new user when a user is registered

--- app/models/user.php
+++ app/models/user.php
@@ -5,24 +5,79 @@ class User
     private $firstName;
     private $lastName;
     private $email;
+    private $password;
     private $dateOfBirth;
-    private $userName;
+    private $displayName;
     private $userType;
     private $externalLink;
     private $profileText;
+    private $lastError;
 
-    public function __construct($firstName, $lastName, $email, $dateOfBirth, $userName, $userType, $externalLink=null, $profileText=null)
+
+    public function __construct($firstName, $lastName, $email,$password, $dateOfBirth, $displayName, $userType, $externalLink=null, $profileText=null)
     {
         $this->firstName = $firstName;
         $this->lastName = $lastName;
         $this->email = $email;
+        $this->password =$password;
         $this->dateOfBirth = $dateOfBirth;
-        $this->userName = $userName;
+        $this->displayName = $displayName;
         $this->userType = $userType;
         $this->externalLink = $externalLink;
         $this->profileText = $profileText;
     }
 
+
+    public function emailExists($databaseCreator)
+    {
+        $email = $this->email;
+        $conn = $databaseCreator->getConnection();
+
+        $stmt = $conn->prepare("SELECT email FROM users WHERE email = ?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $stmt->store_result();
+        $count = $stmt->num_rows;
+        $stmt->close();
+
+        return $count > 0;
+    }
+
+    public function userNameExists($databaseCreator)
+    {
+        $displayName = $this->displayName;
+        $conn = $databaseCreator->getConnection();
+
+        $stmt = $conn->prepare("SELECT displayName FROM users WHERE displayName = ?");
+        $stmt->bind_param("s", $displayName);
+        $stmt->execute();
+        $stmt->store_result();
+        $count = $stmt->num_rows;
+        $stmt->close();
+
+        return $count > 0;
+    }
+
+    public function insertUser($databaseCreator)
+    {
+        $conn = $databaseCreator->getConnection();
+
+        // Use prepared statements to prevent SQL injection
+        $stmt = $conn->prepare("INSERT INTO users (firstName, lastName, email, dateOfBirth, displayName, userType, externalLink, profileText, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssssssss", $this->firstName, $this->lastName, $this->email, $this->dateOfBirth, $this->displayName, $this->userType, $this->externalLink, $this->profileText, $this->password);
+
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            $this->lastError = $stmt->error;
+            return false;
+        }
+    }
+
+    public function getLastError()
+    {
+        return $this->lastError;
+    }
     public function getId()
     {
         return $this->id;
@@ -55,23 +110,33 @@ class User
     {
         $this->email = $email;
     }
-    public function getDateOfBirth()
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+    public function getdateOfBirth()
     {
         return $this->dateOfBirth;
     }
 
-    public function setDateOfBirth($dateOfBirth)
+    public function setdateOfBirth($dateOfBirth)
     {
         $this->$dateOfBirth = $dateOfBirth;
     }
     public function getDisplayName()
     {
-        return $this->userName;
+        return $this->displayName;
     }
 
-    public function setDisplayName($userName)
+    public function setDisplayName($displayName)
     {
-        $this->$userName = $userName;
+        $this->$displayName = $displayName;
     }
     public function getUserType()
     {
